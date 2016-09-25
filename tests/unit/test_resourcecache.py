cbd64de0902a4323aa34ff385ff95e9da3ca27cd
Zach Hill <zach@anchore.com>
2016-09-25 02:07:41 -0700

    Removes old test cases that did not work
    Signed-off-by: Zach Hill <zach@anchore.com>

--- tests/unit/test_resourcecache.py
+++ tests/unit/test_resourcecache.py
@@ -17,14 +17,6 @@ class TestResourceCache (unittest.TestCase):
         print entry
         assert entry['content'] is not None
 
-        entry = cache.get('s3://downloads.anchore/readme.txt')
-        print entry
-        assert entry['content'] is not None
-
-        entry = cache.get('s3://anchore-sync-service/subscriptions/engine.json')
-        print entry
-        assert entry['content'] is not None
-
         print cache.metadata
         cache.clear()
 
@@ -32,46 +24,8 @@ class TestResourceCache (unittest.TestCase):
         cache = ResourceCache(directory='/tmp')
         cache.clear()
 
-        entry = cache.get('s3://downloads.anchore/readme.txt')
-        print entry
-        assert entry['content'] is not None
-
-        entry = cache.get('s3://downloads.anchore/readme.txt')
+        entry = cache.get('http://downloads.anchore.s3-website-us-west-2.amazonaws.com/')
         print entry
         assert entry['content'] is not None
         cache.clear()
-
-    def test_upload(self):
-        cache = ResourceCache(directory='/tmp')
-        cache.clear()
-
-        print 'Testing GET'
-        entry = cache.get('s3://anchore-testing/testkey1')
-        print entry
-        assert entry['content'] is not None
-
-        if not os.path.exists('./testfile'):
-            with open('./testfile', 'w') as f:
-                f.write('Some content here')
-
-        print 'Testing PUT 1'
-        entry = cache.upload('./testfile', 's3://anchore-testing/testkey')
-        print entry
-        assert entry['content'] is not None
-        date1 = entry['last-modified']
-
-        print 'Testing PUT of existing file'
-        entry = cache.upload('./testfile', 's3://anchore-testing/testkey')
-        print entry
-        assert entry['content'] is not None
-        assert date1 == entry['last-modified']
-
-        cache.clear()
-
-
-
-
-
-
-
 
