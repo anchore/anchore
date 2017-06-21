35fdfa3c3ea99a49bf3a0a1e7bfe80ff10a8dd15
Daniel Nurmi <nurmi@anchore.com>
2017-06-21 12:52:00 -0700

    show all packages on cve-scan and cve gate checks instead of collapsed source package only
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

--- anchore/anchore_utils.py
+++ anchore/anchore_utils.py
@@ -1382,7 +1382,6 @@ def cve_load_data(imageId, cve_data_context=None):
         raise ValueError(str(msg))
         #raise ValueError("cannot find CVE data associated with the input container distro: ("+str(distrolist)+")")
 
-
     last_update = 0
     try:
         d = anchore_feeds.load_anchore_feed_group_datameta('vulnerabilities', dstr)
@@ -1597,15 +1596,13 @@ def cve_scan_packages(cve_data, norm_packages, flavor):
                         iversonly = norm_packages['bin_packages'][vpkgname]['version']
                         irelonly = norm_packages['bin_packages'][vpkgname]['release']
                         vpkgs = [vpkgname]
-                    elif vpkgname in norm_packages['src_to_bin']:
+                    
+                    if vpkgname in norm_packages['src_to_bin']:
                         bpkg = norm_packages['src_to_bin'][vpkgname][0]
                         ivers = norm_packages['bin_packages'][bpkg]['fullvers']
                         iversonly = norm_packages['bin_packages'][bpkg]['version']
                         irelonly = norm_packages['bin_packages'][bpkg]['release']
-                        vpkgs = norm_packages['src_to_bin'][vpkgname]
-                    else:
-                        # cve vuln package cannot be mapped to anything installed
-                        pass
+                        vpkgs = vpkgs + norm_packages['src_to_bin'][vpkgname]
 
 
                     # go through all found packages that mapped to the CVE vul package to check versions for vulnerability
