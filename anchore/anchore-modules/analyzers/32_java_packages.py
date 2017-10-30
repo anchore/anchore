faa5dff7c17bdd449e57c14d96820058768588d3
Daniel Nurmi <nurmi@anchore.com>
2017-10-30 14:20:59 -0700

    java package analyzer prototype
    Signed-off-by: Daniel Nurmi <nurmi@anchore.com>

---
+++ anchore/anchore-modules/analyzers/32_java_packages.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import shutil
+import re
+import json
+import time
+import rpm
+import subprocess
+import stat
+import tarfile
+import time
+import hashlib
+import copy
+import traceback
+import pkg_resources
+import zipfile
+
+import anchore.anchore_utils
+
+analyzer_name = "package_list"
+
+try:
+    config = anchore.anchore_utils.init_analyzer_cmdline(sys.argv, analyzer_name)
+except Exception as err:
+    print str(err)
+    sys.exit(1)
+
+imgname = config['imgid']
+imgid = config['imgid_full']
+outputdir = config['dirs']['outputdir']
+unpackdir = config['dirs']['unpackdir']
+
+resultlist = {}
+try:
+    allfiles = {}
+    if os.path.exists(unpackdir + "/anchore_allfiles.json"):
+        with open(unpackdir + "/anchore_allfiles.json", 'r') as FH:
+            allfiles = json.loads(FH.read())
+    else:
+        fmap, allfiles = anchore.anchore_utils.get_files_from_path(unpackdir + "/rootfs")
+        with open(unpackdir + "/anchore_allfiles.json", 'w') as OFH:
+            OFH.write(json.dumps(allfiles))
+
+    for f in allfiles.keys():
+        if allfiles[f]['type'] == 'file':
+            patt = re.match(".*\.(jar|war|ear)", f)
+            if patt:
+                candidate = '/'.join([unpackdir, 'rootfs', f.encode('utf8')])
+                prefix = '/'.join([unpackdir, 'rootfs'])
+                jtype = patt.group(1)
+                #print "JTYPE: " + str(jtype) + " : " + candidate
+
+                if jtype in ['jar', 'war', 'ear']:
+                    try:
+                        name = re.sub("\."+jtype+"$", "", candidate.split("/")[-1])
+
+                        location = f
+                        version = "N/A"
+                        with zipfile.ZipFile(candidate, 'r') as ZFH:
+                            with ZFH.open("META-INF/MANIFEST.MF", 'r') as MFH:
+                                el = {'metadata':{}}
+                                el['location'] = re.sub("^/*"+prefix+"/*", "/", candidate.decode('utf8'))
+                                el['metadata']['MANIFEST.MF'] = MFH.read()
+                                
+                                sname = sversion = svendor = iname = iversion = ivendor = mname = None
+                                for line in el['metadata']['MANIFEST.MF'].splitlines():
+                                    try:
+                                        (k,v) = line.split(": ", 1)
+                                        if k == 'Specification-Title':
+                                            sname = v
+                                        elif k == 'Specification-Version':
+                                            sversion = v
+                                        elif k == 'Specification-Vendor':
+                                            svendor = v
+                                        elif k == 'Implementation-Title':
+                                            iname = v
+                                        elif k == 'Implementation-Version':
+                                            iversion = v
+                                        elif k == 'Implementation-Vendor':
+                                            ivendor = v
+                                    except:
+                                        pass
+
+                                el['name'] = name
+
+                                #if False and mname:
+                                #    el['name'] = mname
+                                #elif iname:
+                                #    el['name'] = iname
+                                #elif sname:
+                                #    el['name'] = sname
+
+                                el['version'] = ""
+                                if sversion:
+                                    el['version'] += sversion
+
+                                if iversion:
+                                    if sversion:
+                                        el['version'] += re.sub("^"+sversion, "", iversion)
+                                    else:
+                                        el['version'] += iversion
+
+                                el['origin'] = "N/A"
+                                if ivendor:
+                                    el['origin'] = ivendor
+                                elif svendor:
+                                    el['origin'] = svendor
+
+                                for k in ['version', 'origin', 'location']:
+                                    if not el[k]:
+                                        el[k] = "N/A"
+                                    
+                                el['type'] = "java-"+str(jtype)
+                                if not el['version'] or el['version'] == "N/A":
+                                    try:
+                                        patt = re.match(".*-([0-9].*)$", el['name'])
+                                        if patt:
+                                            el['version'] = patt.group(1)
+                                    except:
+                                        pass
+                                #el.pop('metadata', None)
+                                #print "EL: " + str(el)
+
+                    except Exception as err:
+                        print "WARN: cannot extract information about discovered jar (" + str(f) + ") - exception: " + str(err)
+                        el = {}
+
+                    if el:
+                        resultlist[el['location']] = json.dumps(el)
+
+except Exception as err:
+    print "WARN: analyzer unable to complete - exception: " + str(err)
+
+if resultlist:
+    ofile = os.path.join(outputdir, 'pkgs.java')
+    anchore.anchore_utils.write_kvfile_fromdict(ofile, resultlist)
+
+sys.exit(0)
---
+++ anchore/anchore-modules/queries/list-java-packages-detail.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import stat
+import re
+import json
+import traceback
+
+import anchore.anchore_utils
+
+# main routine
+
+try:
+    config = anchore.anchore_utils.init_query_cmdline(sys.argv, "params: <package name> <package name> ...\nhelp: use 'all' to show all packages")
+except Exception as err:
+    print str(err)
+    sys.exit(1)
+
+if not config:
+    sys.exit(0)
+
+if len(config['params']) <= 0:
+    print "Query requires input: <package name> <package name> ..."
+
+outlist = list()
+warns = list()
+
+outlist.append(["Image_Id", "Repo_Tags", "Package_Name", "Version", "Origin", "License", "Location"])
+
+try:
+    # handle the good case, something is found resulting in data matching the required columns
+    allimages = {}
+
+    pkgdetail_data = anchore.anchore_utils.load_analysis_output(config['imgid'], 'package_list', 'pkgs.java')
+
+    for pname in pkgdetail_data.keys():
+        jsonstr = pkgdetail_data[pname]
+        pkgdata = json.loads(pkgdetail_data[pname])
+
+        name = pkgdata['name']
+        
+        match = False
+        if 'all' in config['params']:
+            match = True
+        else:
+            for prefix in config['params']:
+                if re.match(prefix, name):
+                    match = True
+                    break
+        
+        if not match:
+            continue
+
+        version = pkgdata.pop('version', 'Unknown')
+        origin = pkgdata.pop('origin', 'N/A')
+        lic = pkgdata.pop('license', 'Unknown')
+        location = pkgdata.pop('location', 'Unknown')
+
+        outlist.append([config['meta']['shortId'], config['meta']['humanname'], name, version, origin, lic, location])
+
+except Exception as err:
+    # handle the case where something wrong happened
+    traceback.print_exc()
+    warns.append("Query failed for image ("+str(config['imgid'])+") with exception: " + str(err))
+
+anchore.anchore_utils.write_kvfile_fromlist(config['output'], outlist)
+if len(warns) > 0:
+    anchore.anchore_utils.write_plainfile_fromlist(config['output_warns'], warns)
+
+sys.exit(0)
+
+
+
